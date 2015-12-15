c6e2be91ed4418ae7630bcb951a7b35ab8b7c4e2
honehhoneh <honey.lyn218@gmail.com>
2015-12-15 18:00:19 +0800

    Prelim exam submission

--- app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
+++ app/src/main/java/tech/sidespell/prelimexam/MainActivity.java
@@ -1,20 +1,80 @@
 package tech.sidespell.prelimexam;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private RadioGroup radioChoice;
+    private RadioButton radioChosen;
+    private Button toggle;
+    private TextView time;
+
+
+
+    private long timeRemaining = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        radioChoice = (RadioGroup) findViewById(R.id.rgrp);
+        toggle = (Button) findViewById(R.id.toggleBtn);
+        time = (TextView) findViewById(R.id.txtTime);
+
+        addListenerOnButton();
+
+
+    }
+
+    private void addListenerOnToggle() {
+
+    }
+
+    public void addListenerOnButton() {
+
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int selectedId = radioChoice.getCheckedRadioButtonId();
+                radioChosen = (RadioButton) findViewById(selectedId);
+
+                String choice = radioChosen.getText().toString();
+
+                if (choice.equals("Increment")) {
+
+                    final Handler handler = new Handler();
+
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            timeRemaining += 100; //increment to seekbar value
+                            time.setText(timeRemaining + "");
+
+                        }
+                    };
+
+                    handler.postDelayed(runnable, 100);
+                }
+
+
+            }
+
+
+        });
+
+
     }
 
     @Override
@@ -37,5 +97,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
     }
 }
